f180463484f19e60fd684a4d651d54e8f4c0734b
Aune <34304992+Guillemain@users.noreply.github.com>
2020-05-03 11:46:25 +0200

    Ajout du son
    VRROOOOOUM

---
+++ Assets/Scripts/Player/EffetsVaisseau.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffetsVaisseau : MonoBehaviour
+{
+
+    [SerializeField] float amplitudePitchVariation = 0.2f;
+    [SerializeField] AudioSource audi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audi.pitch = 1 + (GetComponent<Rigidbody>().velocity.magnitude)/100f * amplitudePitchVariation;
+    }
+}
---
+++ Assets/Scripts/Player/SolDistance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolDistance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position,-transform.up);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo,20f)){
+            print(hitInfo.distance);
+            Debug.DrawLine(transform.position,transform.position - transform.up*hitInfo.distance,Color.blue);
+        }
+    
+
+    }
+}
